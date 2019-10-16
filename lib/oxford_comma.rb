995aa10158a6e7967d656a317d36bedd9b9aaed0
Artiium Arbyummi <artiium21@gmail.com>
2019-10-16 01:36:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,3 +2,8 @@ def oxford_comma(array)
   word = "and"
   array.join(", ")
 end
+
+=begin
+if the array only has two elements, insert "and" between two elements.
+if array has three elements or more insert "and" before the last element.
+=end
